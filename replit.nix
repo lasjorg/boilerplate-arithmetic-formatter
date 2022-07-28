0c791f42f03bfcd523a3dae9673732b107193a63
lasjorg <28780271+lasjorg@users.noreply.github.com>
2022-07-28 19:05:52 +0200

    test: configs

--- replit.nix
+++ replit.nix
@@ -1,19 +1,18 @@
 { pkgs }: {
-  deps = [
-    pkgs.python38Full
-    pkgs.python38Packages.pytest
-  ];
-  env = {
-    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-      # Needed for pandas / numpy
-      pkgs.stdenv.cc.cc.lib
-      pkgs.zlib
-      # Needed for pygame
-      pkgs.glib
-      # Needed for matplotlib
-      pkgs.xorg.libX11
+    deps = [
+        pkgs.python310
     ];
-    PYTHONBIN = "${pkgs.python38Full}/bin/python3.8";
-    LANG = "en_US.UTF-8";
-  };
+    env = {
+        PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            # Needed for pandas / numpy
+            pkgs.stdenv.cc.cc.lib
+            pkgs.zlib
+            # Needed for pygame
+            pkgs.glib
+            # Needed for matplotlib
+            pkgs.xorg.libX11
+        ];
+        PYTHONBIN = "${pkgs.python310}/bin/python3.10";
+        LANG = "en_US.UTF-8";
+    };
 }
